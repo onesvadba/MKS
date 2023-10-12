a0a9379be2de986ed94f02a1cae6108acc555322
onesvadba <xnesva08@vutbr.cz>
2023-10-12 10:35:52 +0200

    new

---
+++ cp03/cvic3/Core/Src/sct.c
@@ -0,0 +1,19 @@
+/*
+ * sct.c
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: xnesva08
+ */
+
+#include "main.h"
+#include "sct.h"
+
+void sct_init(void)
+{
+	sct_led(0);
+}
+
+void sct_led(uint32_t value)
+{
+
+}
