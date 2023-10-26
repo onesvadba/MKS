408edc1394296ef3211b8a78a3b416e56b4a348a
onesvadba <xnesva08@vutbr.cz>
2023-10-26 11:14:44 +0200

    UPDATE

--- cp05/cvic5/Core/Inc/main.h
+++ cp05/cvic5/Core/Inc/main.h
@@ -63,8 +63,12 @@ void Error_Handler(void);
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
+#define LED1_Pin GPIO_PIN_4
+#define LED1_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_0
+#define LED2_GPIO_Port GPIOB
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
